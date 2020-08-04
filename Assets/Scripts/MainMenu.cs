4f32e53b6612216e8ccdd58d7eaab5dc4499c0b6
cvhnilicka <cvhnilicka@wisc.edu>
2020-08-03 18:53:26 -0700

    getting the basics of the menu working; can exit and start game. Will want to eventually get to a level menu as well

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        print("start game");
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+        print("exit game");
+        Application.Quit();
+    }
+
+}
---
+++ Assets/Scripts/MouseHover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MouseHover : MonoBehaviour
+{
+	
+
+	void Start()
+	{
+	}
+
+	void OnMouseEnter()
+	{
+		GetComponent<Image>().material.color = Color.red;
+	}
+
+	void OnMouseExit()
+	{
+		GetComponent<Image>().material.color = Color.black;
+	}
+}
